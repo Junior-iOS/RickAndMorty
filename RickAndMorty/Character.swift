df32550c0d0e16b55da9088af6cbb085e0ae0199
NJ Development <nj.development83@gmail.com>
2022-12-28 18:38:24 -0300

    Models created

--- RickAndMorty/Character.swift
+++ RickAndMorty/Character.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+
+struct Character: Codable {
+    let id: Int
+    let name: String
+    let status: CharacterStatus
+    let species: String
+    let type: String
+    let gender: CharacterGender
+    let origin: CharacterOrigin
+    let location: CharacterLocation
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+enum CharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case `unknown` = "unknown"
+}
+
+enum CharacterGender: String, Codable {
+    case male = "Male"
+    case female = "Femae"
+    case genderless = "Genderless"
+    case `unknown` = "unknown"
+}
+
+struct CharacterOrigin: Codable {
+    let name: String
+    let url: String
+}
+
+struct CharacterLocation: Codable {
+    let name: String
+    let url: String
+}
