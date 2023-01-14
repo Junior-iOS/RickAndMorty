1cb1ffd1596fa971dd30c0ca3539802dc14a1283
NJ Development <nj.development83@gmail.com>
2023-01-14 00:43:05 -0300

    CharacterInfoCell populated

--- RickAndMorty/CharacterInfoCollectionViewCell.swift
+++ RickAndMorty/CharacterInfoCollectionViewCell.swift
@@ -10,10 +10,40 @@ import UIKit
 class CharacterInfoCollectionViewCell: UICollectionViewCell {
     static let identifier = "CharacterInfoCollectionViewCell"
     
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 22, weight: .medium)
+        return label
+    }()
+    
+    private let valueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 20, weight: .light)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let icon = UIImageView()
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.contentMode = .scaleAspectFit
+        return icon
+    }()
+    
+    private let titleContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .secondarySystemBackground
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .tertiarySystemBackground
-        contentView.layer.cornerRadius = 9
+        
+        setup()
     }
     
     required init?(coder: NSCoder) {
@@ -21,10 +51,67 @@ class CharacterInfoCollectionViewCell: UICollectionViewCell {
     }
     
     override func prepareForReuse() {
-        
+        titleLabel.text = nil
+        titleLabel.textColor = .label
+        valueLabel.text = nil
+        iconImageView.image = nil
+        iconImageView.tintColor = .label
     }
     
     public func configure(with viewModel: CharacterInfoCollectionViewCellViewModel) {
+        titleLabel.text = viewModel.title
+        valueLabel.text = viewModel.displayValue
         
+        switch valueLabel.text {
+        case "Alive":
+            iconImageView.image = UIImage(systemName: "waveform.path.ecg")
+            iconImageView.tintColor = .systemGreen
+            titleLabel.textColor = .systemGreen
+        case "Dead":
+            iconImageView.image = UIImage(systemName: "xmark.circle")
+            iconImageView.tintColor = .systemRed
+            titleLabel.textColor = .systemRed
+        default:
+            iconImageView.image = viewModel.iconImage
+            iconImageView.tintColor = viewModel.tintColor
+            titleLabel.textColor = viewModel.tintColor
+        }
+    }
+}
+
+extension CharacterInfoCollectionViewCell: CodeView {
+    func buildHierarchy() {
+        contentView.addSubviews(titleContainerView, valueLabel, iconImageView)
+        titleContainerView.addSubview(titleLabel)
+    }
+    
+    func setupConstraints() {
+        titleContainerView.anchor(left: contentView.leftAnchor,
+                                  bottom: contentView.bottomAnchor,
+                                  right: contentView.rightAnchor)
+        NSLayoutConstraint.activate([titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33)])
+        
+        titleLabel.anchor(top: titleContainerView.topAnchor,
+                          left: titleContainerView.leftAnchor,
+                          bottom: titleContainerView.bottomAnchor,
+                          right: titleContainerView.rightAnchor)
+        
+        iconImageView.anchor(top: contentView.topAnchor,
+                             paddingTop: 33,
+                             left: contentView.leftAnchor,
+                             paddingLeft: 15)
+        iconImageView.setDimensions(height: 30, width: 30)
+        
+        valueLabel.anchor(left: iconImageView.rightAnchor,
+                          paddingLeft: 10,
+                          right: contentView.rightAnchor,
+                          paddingRight: 10)
+        valueLabel.centerY(inView: iconImageView)
+    }
+    
+    func setupAdditionalConfiguration() {
+        contentView.backgroundColor = .tertiarySystemBackground
+        contentView.layer.cornerRadius = 9
+        contentView.layer.masksToBounds = true
     }
 }
--- RickAndMorty/CharacterInfoCollectionViewCellViewModel.swift
+++ RickAndMorty/CharacterInfoCollectionViewCellViewModel.swift
@@ -5,16 +5,121 @@
 //  Created by Junior Silva on 13/01/23.
 //
 
-import Foundation
+import UIKit
 
 final class CharacterInfoCollectionViewCellViewModel {
     
-    public let value: String
-    public let title: String
+    private let value: String
+    private let type: `Type`
     
-    init(value: String, title: String) {
+    public var title: String {
+        self.type.displayTitle
+    }
+    
+    static let dateFormatter: DateFormatter =  {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSZ"
+        formatter.timeZone = .current
+        return formatter
+    }()
+    
+    static let shortDateFormatter: DateFormatter =  {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        formatter.timeZone = .current
+        return formatter
+    }()
+    
+    public var displayValue: String {
+        if value.isEmpty { return "None"}
+        
+        if let date = Self.dateFormatter.date(from: value), type == .created {
+            return Self.shortDateFormatter.string(from: date)
+        }
+        
+        return value.capitalized
+    }
+    
+    public var iconImage: UIImage? {
+        return type.iconImage
+    }
+    
+    public var tintColor: UIColor {
+        return type.tintColor
+    }
+    
+    enum `Type`: String {
+        case status
+        case gender
+        case type
+        case species
+        case origin
+        case location
+        case created
+        case episode
+        
+        var tintColor: UIColor {
+            switch self {
+            case .status:
+                return .systemYellow
+            case .gender:
+                return .systemBlue
+            case .type:
+                return .systemCyan
+            case .species:
+                return .systemMint
+            case .origin:
+                return .systemOrange
+            case .location:
+                return .systemPink
+            case .created:
+                return .systemPurple
+            case .episode:
+                return .systemBrown
+            }
+        }
+        
+        var iconImage: UIImage? {
+            switch self {
+            case .status:
+                return UIImage(systemName: "questionmark.square.dashed")
+            case .gender:
+                return UIImage(systemName: "figure.dress.line.vertical.figure")
+            case .type:
+                return UIImage(systemName: "person.and.background.dotted")
+            case .species:
+                return UIImage(systemName: "bonjour")
+            case .origin:
+                return UIImage(systemName: "globe.americas")
+            case .location:
+                return UIImage(systemName: "airplane")
+            case .created:
+                return UIImage(systemName: "clock")
+            case .episode:
+                return UIImage(systemName: "tv")
+            }
+        }
+        
+        var displayTitle: String {
+            switch self {
+            case .status,
+                 .gender,
+                 .type,
+                 .species,
+                 .origin,
+                 .location,
+                 .created:
+                return rawValue.capitalized
+            case .episode:
+                return "Episodes"
+            }
+        }
+    }
+    
+    init(type: `Type`, value: String) {
+        self.type = type
         self.value = value
-        self.title = title
     }
     
 }
